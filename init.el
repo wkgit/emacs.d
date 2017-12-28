1b8aaffe4bef020581100bbc81a7344681e74359
wkgit <wk-git@163.com>
2017-12-28 19:21:24 +0800

    modify

--- init.el
+++ init.el
@@ -22,7 +22,7 @@
 ;;注：新版改成使用0，旧版使用nil的做法已经失效，但(set-scroll-bar-mode nil)可以使用
 
 ; 关闭启动画面
-;(setq inhibit-startup-message t)
+(setq inhibit-startup-message t)
 
 ;highlight当前行
 (global-hl-line-mode 1)
